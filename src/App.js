b83c69db0eb44fd54123cf2d1304e931f4ad4e58
rahulrs1399 <sarojrahul1399@gmail.com>
2023-12-26 17:24:31 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {data} from './data.js';
 
 function App() {
+
+  const [search, setSearch] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <h1 className='text-center mt-4'>Contact Keeper</h1>
+        <Form>
+          <InputGroup className='my-3'>
+            <Form.Control 
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search contacts' 
+            />
+          </InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              .filter((item) => {
+                return search.toLowerCase() === ''
+                ? item
+                : Object.values(item).join('').toLowerCase().includes(search.toLowerCase());
+              })
+              .map((item) => (
+              <tr key={item.id}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
